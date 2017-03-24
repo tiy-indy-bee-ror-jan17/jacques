7269a138917f19ac098896d00d97af20dbf459a5
Ben Call <bgcall@indiana.edu>
2017-03-24 12:20:49 -0400

    built seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+
+  username = Faker::Internet.user_name
+
+  user = User.create!(
+    username: username,
+    email: Faker::Internet.safe_email,
+    password: Faker::Internet.password,
+  )
+
+  5.times do
+    user.notes.create!(
+      title: Faker::Beer.name
+      body: Faker::Coffee.notes
+    )
+  end
+
+end
