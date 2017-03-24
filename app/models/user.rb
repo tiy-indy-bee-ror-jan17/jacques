e4a587f6906f4eb316e6ce9c7915b42361ab7ff8
klocode <kendrick.t.lo@gmail.com>
2017-03-24 12:05:33 -0400

    seeding database

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :api_token
 
+  validates :username, :email, presence: true
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |x|
+  user = User.create!(
+  username: Faker::Pokemon.name,
+  email: Faker::Internet.safe_email,
+  password: "password"
+  )
+  3.times do
+    note = user.notes.create!(
+    title: Faker::LordOfTheRings.location,
+    body: Faker::Company.catch_phrase
+    )
+    3.times do
+      note.tags.find_or_create_by!(
+      name: Faker::Commerce.color
+      )
+    end
+  end
+end
