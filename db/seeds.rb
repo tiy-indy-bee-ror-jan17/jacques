27af7037b4a0a938d6196e0fccd914632c1d96ce
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-03-24 14:39:29 -0400

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do
+5.times do |u|
   user = User.create!(
   username: Faker::Beer.name,
   email: Faker::Internet.email,
