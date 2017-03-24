26ad09496782727656dede5192e88fc37e380649
klocode <kendrick.t.lo@gmail.com>
2017-03-24 15:45:27 -0400

    debugging

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def show
-    @tag = Tag.find(params[:tag])
+    @tag = Tag.find(params[:name])
     render json: @tag, include: ['notes.tags', 'notes.user']
   end
 
