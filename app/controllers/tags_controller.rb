29cbef3cf7ea618181c591b2333eeb9f04c85bfe
klocode <kendrick.t.lo@gmail.com>
2017-03-24 15:42:15 -0400

    changing find by to find

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def show
-    @tag = Tag.find_by(name: params[:tag])
+    @tag = Tag.find(name: params[:tag])
     render json: @tag, include: ['notes.tags', 'notes.user']
   end
 
