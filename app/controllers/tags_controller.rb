552d057b259d4bc6a5e7749682b5bad5e91d64fa
klocode <kendrick.t.lo@gmail.com>
2017-03-24 15:46:52 -0400

    reverting changes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def show
-    @tag = Tag.find(params[:name])
+    @tag = Tag.find_by(name: params[:tag])
     render json: @tag, include: ['notes.tags', 'notes.user']
   end
 
