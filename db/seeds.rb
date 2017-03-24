90e85b04fb8a818c9a4bdd4baed57a2d74c239a7
Rob <epr1935@icloud.com>
2017-03-24 14:09:40 -0400

    in config/seed.rb, created 3 notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def setup
+  3.times do
+    FactoryGirl.create(:note_with_tags)
+  end
+end
+
+setup
