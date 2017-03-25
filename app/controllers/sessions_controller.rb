e99baee8533200bb44e74d5a0b07fcedc3e838cb
Michael Stashevsky <mstashev@gmail.com>
2017-03-25 17:20:08 -0400

    changed sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:username])&.authenticate(params[:password])
     if @user
       session[:user_id] = @user.id
-        flash[:success] = "Welcome, #{@user.name}"
+        # flash[:success] = "Welcome, #{@user.name}"
       redirect_to :index
     else
-      flash[:danger] = "Username or password is incorrect. Please try again."
+      # flash[:danger] = "Username or password is incorrect. Please try again."
       redirect_to :login
     end
   end
