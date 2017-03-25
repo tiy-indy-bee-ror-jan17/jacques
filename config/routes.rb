82cf6137340c7b3d708e8d4a638b98395c106661
Michael Stashevsky <mstashev@gmail.com>
2017-03-25 14:09:51 -0400

    added Name column to users

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,10 @@ Rails.application.routes.draw do
   resources :notes
   resources :users
 
-  get '/api/notes.json' => 'notes#index'
-  get '/api/notes'  => 'notes#index'
-  post '/api/users'   => 'users#create'
-  post '/api/notes' => 'notes#create'
-  get '/api/notes/tag/:name' => 'tags#show'
-
-
-
+  get  '/api/notes.json'      => 'notes#index'
+  get  '/api/notes'           => 'notes#index'
+  post '/api/users'           => 'users#create'
+  post '/api/notes'           => 'notes#create'
+  get  '/api/notes/tag/:name' => 'tags#show'
 
 end
