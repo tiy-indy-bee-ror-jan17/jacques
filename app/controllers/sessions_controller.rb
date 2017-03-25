5e323d601da8c221d765f46a892786debb64b78e
Michael Stashevsky <mstashev@gmail.com>
2017-03-25 17:16:56 -0400

    changed authentication vars

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:session][:username])&.authenticate(params[:session][:password])
+    @user = User.find_by(username: params[:username])&.authenticate(params[:password])
     if @user
       session[:user_id] = @user.id
         flash[:success] = "Welcome, #{@user.name}"
