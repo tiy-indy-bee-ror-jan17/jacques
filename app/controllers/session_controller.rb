308fee81c6fd540760b167a66447c87c9f5b6b13
Michael Stashevsky <mstashev@gmail.com>
2017-03-25 16:27:09 -0400

    added a sessions controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,21 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:session][:username])&.authenticate(params[:session][:password])
+    if @user
+      session[:user_id] = @user.id
+        flash[:success] = "Welcome, #{@user.name}"
+      redirect_to :index
+    else
+      flash[:danger] = "Username or password is incorrect. Please try again."
+      redirect_to :login
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to :root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   resources :notes
   resources :users
 
-  get  '/api/notes.json'      => 'notes#index'
-  get  '/api/notes'           => 'notes#index'
-  post '/api/users'           => 'users#create'
-  post '/api/notes'           => 'notes#create'
-  get  '/api/notes/tag/:name' => 'tags#show'
-
+  get    '/api/notes.json'      => 'notes#index'
+  get    '/api/notes'           => 'notes#index'
+  post   '/api/users'           => 'users#create'
+  post   '/api/notes'           => 'notes#create'
+  get    '/api/notes/tag/:name' => 'tags#show'
+  get    '/api/login'           => 'sessions#new',       as: :login
+  post   '/api/login'           => 'sessions#create'
+  delete '/api/logout'          => 'sessions#destroy',   as: :logout
 end
