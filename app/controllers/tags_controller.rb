7b9b11fb0f9e512f7b418f9d43056c5c875a8286
Ben Call <bgcall@indiana.edu>
2017-03-26 10:18:12 -0400

    changed find by to where in tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def index
-    @tag = Tag.find_by(name: params[:tag])
+    @tag = Tag.where(name: params[:tag])
     render json: @tag, include: ['notes.tags', 'notes.user']
   end
 
