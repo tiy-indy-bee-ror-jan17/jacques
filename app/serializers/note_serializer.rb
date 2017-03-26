68a670537fdcde649b1967e84933bc593f2361cc
CosmicCandi <kalea.wolff@gmail.com>
2017-03-26 15:40:37 -0400

    Removes default user piece causing breakages.

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -4,15 +4,4 @@ class NoteSerializer < ActiveModel::Serializer
   has_many :tags
   belongs_to :user
 
-  before_action :default_user, only: [:createf]
-
-  def default_user
-    @note = Note.find_by(username: params[:username])
-    if @note.user.username.nil
-      note.user.username = "anonymous"
-    else
-      @note.user.username
-    end
-  end
-
 end
