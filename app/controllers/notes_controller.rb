95227dd88d59eb13ab7497e54603b8748113458f
Ben Call <bgcall@indiana.edu>
2017-03-26 12:11:34 -0400

    commented out my tags split in notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,10 +13,10 @@ class NotesController < ApplicationController
     @note = Note.new(note_params)
     #Typically move this bottom section to a model to keep the controller light
     if @note.save
-      params[:tags].split(',').each do |name|
-        tag = Tag.find_or_create_by(name: name)
-        @note.tags << tag
-      end
+      # params[:tags].split(',').each do |name|
+      #   tag = Tag.find_or_create_by(name: name)
+      #   @note.tags << tag
+      # end
       render json: @note
     else
       render json: {errors: @note.errors.full_messages.collect{ |e|{error: e}}}, status: 400
